0f54a9427af197a392c13067cb33971249d778de
Chris Haralson <ocarmike@sonic.com>
2015-11-06 20:00:00 -0600

    warmup

---
+++ valueReturnFunction/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int sum(int num1, int num2);
+
+int main()
+{
+	int value1 = 20,
+		value2 = 40,
+		total;
+
+	total = sum(value1, value2);
+
+	cout << "The sum of " << value1 << " and "
+		<< value2 << " is " << total << endl;
+	system("pause");
+	return 0;
+}
+
+int sum(int num1, int num2)
+{
+	return num1 + num2;
+	
+}
